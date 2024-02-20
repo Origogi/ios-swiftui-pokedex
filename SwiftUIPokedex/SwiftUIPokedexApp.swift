36ac01a4e6f0ba7b52579f08fe1c6827b46bd784
tony.origogi <tony.origogi@kakaobrain.com>
2024-02-20 22:23:45 +0900

    Initial Commit

---
+++ SwiftUIPokedex/SwiftUIPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPokedexApp.swift
+//  SwiftUIPokedex
+//
+//  Created by 김정태 on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
