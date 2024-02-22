8c70444c1536128eca030accd199366fc19113c5
tony.origogi <tony.origogi@kakaobrain.com>
2024-02-22 21:16:23 +0900

    Navigation Button 추가

---
+++ SwiftUIPokedex/Components/NavigationButton.swift
@@ -0,0 +1,65 @@
+//
+//  NavigationButton.swift
+//  SwiftUIPokedex
+//
+//  Created by 김정태 on 2/22/24.
+//
+
+import SwiftUI
+
+
+
+struct NavigationButton: View {
+  let foregroundColor: Color
+  let onImagePath : String
+  let offImagePath : String
+  let title : String
+  
+  @State var isActivated: Bool = true
+  
+  init(
+    foregroundColor: Color,
+    onImagePath: String,
+    offImagePath: String,
+    title: String
+  ) {
+    self.foregroundColor = foregroundColor
+    self.onImagePath = onImagePath
+    self.offImagePath = offImagePath
+    self.title = title
+  }
+  
+  var body: some View {
+    ZStack(alignment: .top) {
+      Rectangle()
+        .fill(foregroundColor)
+        .frame(width: 56,height: 56)
+      Image(isActivated ? onImagePath : offImagePath)
+        .resizable()
+        .frame(width: 26, height: 26)
+        .offset(y : isActivated ? 7 : 15)
+      Text(title)
+        .font(.custom("Poppins-Medium", size: 12))
+        .foregroundColor(Color(hex: "173EA5"))
+        .offset(y : 31)
+        .opacity(isActivated ? 1 : 0)
+    }.onTapGesture {
+      withAnimation {
+        isActivated.toggle()
+      }
+    }
+  }
+}
+
+#Preview {
+  NavigationButton(
+  
+    foregroundColor: Color.white, onImagePath: "PokedexOn", offImagePath: "PokedexOff", title: "Pokedex"
+  )
+}
+
+#Preview {
+  NavigationButton(
+    foregroundColor: Color.white, onImagePath: "RegionOn", offImagePath: "RegionOff", title: "Region"
+  )
+}
