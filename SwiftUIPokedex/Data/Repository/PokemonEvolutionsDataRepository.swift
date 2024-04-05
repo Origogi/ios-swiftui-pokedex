d6d5991e48686435d9c8bf853b93d9fcc9e8d4be
tony.origogi <tony.origogi@kakaobrain.com>
2024-04-05 16:26:18 +0900

    데이터 추가

--- SwiftUIPokedex/Data/Repository/PokemonEvolutionsDataRepository.swift
+++ SwiftUIPokedex/Data/Repository/PokemonEvolutionsDataRepository.swift
@@ -20,6 +20,27 @@ class PokemonEvolutiosDataRepository {
     self.dict[1] = dataBulbasaur
     self.dict[2] = dataBulbasaur
     self.dict[3] = dataBulbasaur
+    
+    let dataCharmander = EvolutionsData(chains: [
+      EvolutionChainData(next: 4),
+      EvolutionChainData(next: 5, condition: "Level 16"),
+      EvolutionChainData(next: 6, condition: "Level 32")
+    ])
+    
+    self.dict[4] = dataCharmander
+    self.dict[5] = dataCharmander
+    self.dict[6] = dataCharmander
+    
+    let dataSquirtle = EvolutionsData(chains: [
+      EvolutionChainData(next: 7),
+      EvolutionChainData(next: 8, condition: "Level 16"),
+      EvolutionChainData(next: 9, condition: "Level 32")
+    ])
+    
+    self.dict[7] = dataSquirtle
+    self.dict[8] = dataSquirtle
+    self.dict[9] = dataSquirtle
+
   }
   
   func getByPokemonId(pokemonId: Int) -> EvolutionsData {
