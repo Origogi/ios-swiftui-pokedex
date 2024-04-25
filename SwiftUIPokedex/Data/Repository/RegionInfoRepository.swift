ae0f88277784a23e247b96c15668d832175590a1
tony.origogi <tony.origogi@kakaobrain.com>
2024-04-25 17:54:20 +0900

    데이터 추가

--- SwiftUIPokedex/Data/Repository/RegionInfoRepository.swift
+++ SwiftUIPokedex/Data/Repository/RegionInfoRepository.swift
@@ -10,10 +10,10 @@ import Foundation
 class RegionInfoRepository {
   private let data = [
     RegionInfo(bgImagePath: "kanto", regionName: "Kanto", generationNumer: 1, startingPokemonImagePath: ["1_small", "4_small", "7_small"]),
-    RegionInfo(bgImagePath: "johto", regionName: "Johto", generationNumer: 2, startingPokemonImagePath: ["1_small", "4_small", "7_small"]),
-    RegionInfo(bgImagePath: "hoenn", regionName: "Hoenn", generationNumer: 3, startingPokemonImagePath: ["1_small", "4_small", "7_small"]),
-    RegionInfo(bgImagePath: "sinnoh", regionName: "Sinnoh", generationNumer: 4, startingPokemonImagePath: ["1_small", "4_small", "7_small"]),
-    RegionInfo(bgImagePath: "unova", regionName: "Unova", generationNumer: 5, startingPokemonImagePath: ["1_small", "4_small", "7_small"]),
+    RegionInfo(bgImagePath: "johto", regionName: "Johto", generationNumer: 2, startingPokemonImagePath: ["152_small", "155_small", "158_small"]),
+    RegionInfo(bgImagePath: "hoenn", regionName: "Hoenn", generationNumer: 3, startingPokemonImagePath: ["252_small", "255_small", "258_small"]),
+    RegionInfo(bgImagePath: "sinnoh", regionName: "Sinnoh", generationNumer: 4, startingPokemonImagePath: ["387_small", "390_small", "393_small"]),
+    RegionInfo(bgImagePath: "unova", regionName: "Unova", generationNumer: 5, startingPokemonImagePath: ["495_small", "498_small", "501_small"]),
     RegionInfo(bgImagePath: "kalos", regionName: "Kalos", generationNumer: 6, startingPokemonImagePath: ["1_small", "4_small", "7_small"]),
     RegionInfo(bgImagePath: "alola", regionName: "Alola", generationNumer: 7, startingPokemonImagePath: ["1_small", "4_small", "7_small"]),
     RegionInfo(bgImagePath: "galar", regionName: "Galar", generationNumer: 8, startingPokemonImagePath: ["1_small", "4_small", "7_small"])
